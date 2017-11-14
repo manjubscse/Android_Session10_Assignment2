6c1595c7ca1a484136cbcff24ed994b07edb2c7c
Manju B S <manjubscse@gmail.com>
2017-11-14 11:03:55 +0530

    Assignment10.2

---
+++ app/src/main/java/manjubs/listFragments/Fragment02.java
@@ -0,0 +1,19 @@
+package manjubs.listFragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment02 extends Fragment {
+    View rootView;
+    MainActivity activity = (MainActivity) getActivity();
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /** Inflating the layout for this fragment **/
+        rootView = inflater.inflate(R.layout.my_fragment02, container, false);
+
+        return rootView;
+    }
+}
